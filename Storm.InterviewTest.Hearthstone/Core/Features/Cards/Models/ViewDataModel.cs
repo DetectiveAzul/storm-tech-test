9e78c6d74e314d463c8d3b770d3bc8582fd69353
DetectiveAzul <detec.azul@gmail.com>
2018-06-19 11:54:00 +0100

    Created ViewDataModel class to pass more than one set of information to Card browser view. Refactored Dropdown menu to use a for loop to populate instead of hardcoding the information.

---
+++ Storm.InterviewTest.Hearthstone/Core/Features/Cards/Models/ViewDataModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Storm.InterviewTest.Hearthstone.Core.Features.Cards.Domain;
+
+//This class was created to group all the information I want to pass to the Card Browser view to simplify
+//how we send information to the view (that only accepts one parameter now)
+namespace Storm.InterviewTest.Hearthstone.Core.Features.Cards.Models
+{
+    public class ViewDataModel
+    {
+        public IEnumerable<CardModel> CardsData { get; set; }
+        public IEnumerable<CardModel> HeroesData { get; set;  }
+
+        public ViewDataModel()
+        {
+
+        }
+
+    }
+}
