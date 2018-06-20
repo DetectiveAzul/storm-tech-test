939a1f95ce2136fb3fc159e998ac4a520b85ef7c
DetectiveAzul <detec.azul@gmail.com>
2018-06-20 10:23:07 +0100

    Added basic empty class MediaRetrievalService.cs

---
+++ Storm.InterviewTest.Hearthstone/Core/Features/Cards/Services/MediaRetrievalService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Storm.InterviewTest.Hearthstone.Core.Features.Cards.Services
+{
+    public class MediaRetrievalService
+    {
+    }
+}
